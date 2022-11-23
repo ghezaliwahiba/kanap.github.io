3597e30cd93f197a1cd7f7405e117d18975bb3cd
Wahiba GHEZALI RAMDANI <113927495+ghezaliwahiba@users.noreply.github.com>
2022-11-23 17:32:11 +0100

    Update product.js

--- P5-Dev-Web-Kanap/front/js/product.js
+++ P5-Dev-Web-Kanap/front/js/product.js
@@ -1,40 +1,36 @@
 let urlSearch = window.location.search; //permet de récupérer l’URL du page courante.
-let urlParams= new URLSearchParams (urlSearch)
-let idProduct = urlParams.get ("id")
-console.log(idProduct);
+let urlParams= new URLSearchParams (urlSearch);
+let id = urlParams.get ("id");
 
 
-fetch(`http://localhost:3000/api/products/${idProduct}`) //string interpolation
+fetch(`http://localhost:3000/api/products/${id}`) 
       .then((response) => response.json())
        
-      .then((res) => AddKanap (res)) 
+      .then((res) => AddKanap(res)) 
 
-function AddKanap (product) {
-       
-    const imageUrl = product.imageUrl;
-    const altTxt = product.altTxt;
-    const colors = product.colors;
-    const description = product.description;
-    const name = product.name;
-    const _id = product._id;
-    const price = product.price;
+function AddKanap (product) { 
 
-    const image=document.createElement("img")
+    let imageUrl = product.imageUrl;
+    let altTxt = product.altTxt;
+    let description = product.description;
+    let name = product.name;
+  
+    const image=document.createElement("img");
     image.src= imageUrl;
-    image.alt=altTxt;
+    image.alt=altTxt; 
     const itemme =document.querySelector(".item__img");
     itemme.appendChild(image);
-
+   
     const para =document.querySelector("#title"); 
     para.textContent=name;
 
-    const text = document.querySelector("#price")
+    const text = document.querySelector("#price");
     text.innerHTML=price;
 
-    const descpt = document.querySelector ("#description")
+    const descpt = document.querySelector ("#description");
     descpt.innerHTML=description;
 
-   let ColorsProduct= product.colors;
+    let ColorsProduct= product.colors;
     ColorsProduct.forEach (function (color) {
         console.log(color);
         let productColor = document.createElement("option");
@@ -42,4 +38,45 @@ function AddKanap (product) {
         productColor.innerText = color;
         item.appendChild(productColor);
     });
+    
+
+  
+
+const button = document.querySelector('#addToCart') // On récupère l'élément sur lequel on veut détecter le clic
+button.addEventListener('click', function () {  // On écoute l'événement click
+let colors = document.querySelector("#colors").value;
+let quantity = document.querySelector("#quantity").value;
+
+if (colors == '' || quantity == 0) {
+alert ('Please select a color or quantity!') 
+return
+
+}
+else if (colors == '' || quantity <0){
+    alert ('Please select a quantity between 0 and 100!')
+    return 
+}
+
+else if (colors == '' || quantity >100){
+    alert ('Please select a quantity between 0 and 100!') 
+    return
+}
+
+let ProductStorage = {
+    id: id,
+    colors: colors,
+    quantity: quantity,
+    price: price,
+    imageUrl:imageUrl,
+    altTxt:altTxt,
+    name:name
+
+}
+
+// le local storage ne stocke pas des objets, ne stocke que des valeurs sous forme de chaines de caractères
+//on utilisera le format JSON et on sérialise l'objet avec la synthaxe JSON.stringify(). cette operation transforme l'objet en une chaine de caractère.
+localStorage.setItem(id, JSON.stringify(ProductStorage));
+window.location.href="cart.html";
+});
+
 }
