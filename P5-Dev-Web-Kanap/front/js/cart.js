98726479d775f082df26b51bf84d1d97a3d98516
Wahiba GHEZALI RAMDANI <113927495+ghezaliwahiba@users.noreply.github.com>
2022-11-28 10:58:41 +0100

    Update cart.js

--- P5-Dev-Web-Kanap/front/js/cart.js
+++ P5-Dev-Web-Kanap/front/js/cart.js
@@ -1,47 +1,36 @@
-/*
-// pour obtenir le nombre de paires clé/valeur:
-localStorage.length()
-// pour obtenir le nom de la clé: 
-localStorage.key(0)
-//pour obtenir toutes les clés:
-for (let i=o; i<localStorage.length; i++){
-  localStorage.key(i);
-}
-*/
-
-
-let urlSearchCart = window.location.search; 
-let urlParamsCart= new URLSearchParams (urlSearchCart);
-let id = urlParams.get ("id");
+let urlSearchCart=window.location.search; 
+let urlParamsCart=new URLSearchParams (urlSearchCart);
+let id=urlParams.get ("id");
 let ProductLocalStorage;
-const url = "http://localhost:3000/api/products/";
+const url="http://localhost:3000/api/products/";
+
+  // Si le localstorage est vide
 
 if ( localStorage.getItem(ProductStorage) !=null ){
   ProductLocalStorage = JSON.parse(ProductLocalStorage);
-}
+} 
 else {
-  ProductLocalStorage = []; // inisialiser un tableau (tableau vide)
+  ProductLocalStorage = []; // initialiser un tableau (tableau vide)
 }
 
 function GetProductLocalStorage(){
 
+// Si le localstorage contient des produits
   for(let i=0; i< ProductLocalStorage.length; i++) {
     
-    fetch(url+ ProductLocalStorage[i].id) // on veut dans l'APi le produit qui correspond à l'id stoqué dans le local storage.
+    fetch(url+ ProductLocalStorage[i].id) // on veut reccuperer dans l'APi le produit qui correspond à l'id stoqué dans le local storage.
       .then((response) => response.json())
-       
       .then((res) => data(res)) 
+    
+      function data (product) { 
 
-  
-
-    const NewArticle = document.createElement('article')
-    const CartItems = document.querySelector ('#cart__items')
-    CartItems.appendChild(NewArticle)
-
-
+      const NewArticle = document.createElement('article')
+      const CartItems = document.querySelector ('#cart__items')
+      CartItems.appendChild(NewArticle)
+    
       const DIV = document.createElement('div')
       NewArticle.appendChild(DIV)
-
+    
       const CartItemImg = document.createElement("img");
       CartItemImg.src=product.imageUrl;
       CartItemImg.alt=product.altTxt;
@@ -49,12 +38,12 @@ function GetProductLocalStorage(){
 
       const CartItemContent = document.createElement('div')
       NewArticle.appendChild(CartItemContent)
-
+     
       const CartItemContentDescription = document.createElement('div')
       CartItemContent.appendChild(CartItemContentDescription)
-
+    
       const ProductNameCart= document.createElement ("h2");
-      ProductNameCart.innerText=product.name; 
+      ProductNameCart.innerText=product.name;
       CartItemContentDescription.appendChild(ProductNameCart);
 
       const ProductColorCart= document.createElement ("P");
@@ -64,24 +53,27 @@ function GetProductLocalStorage(){
       const ProductPriceCart= document.createElement ("P");
       ProductPriceCart.innerText =product.price;
       CartItemContentDescription.appendChild(ProductPriceCart);
-
+     
       const cartItemContentSettings = document.createElement('div')
       CartItemContent.appendChild(cartItemContentSettings)
 
       const cartItemContentSettingsQuantity = document.createElement('div')
       cartItemContentSettings.appendChild(cartItemContentSettingsQuantity)
-
+     
       const ProductQuantityCart= document.createElement ("P");
-      ProductQuantityCart.innerText = "quantity";
+      ProductQuantityCart.innerText = product.quantity;
       cartItemContentSettingsQuantity.appendChild(ProductQuantityCart);
 
+      const input = document.createElement ("input");
+      ProductQuantityCart.appendChild(ProductQuantityCart);
+    
       const cartItemContentSettingsDelete = document.createElement('div')
       cartItemContentSettings.appendChild(cartItemContentSettingsDelete);
-
 
       const ProductDeleteCart= document.createElement ("P");
       ProductDeleteCart.innerText = "Supprimer";
       cartItemContentSettingsDelete.appendChild(ProductDeleteCart);
      
-}
+    }
+    } 
 }
